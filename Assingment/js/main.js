03d4ce6813a279622f76c3cad3db411ec884d0af
Andre Henrique Adura <38469671+AndreAdura@users.noreply.github.com>
2018-05-14 19:04:52 -0700

    Add files via upload

---
+++ Assingment/js/main.js
@@ -0,0 +1,11 @@
+// GAME 1
+function(){
+	document.getElementById('name').innerHTML = "Hello " + input('name') + ", Nice work! Let’s start!";
+}
+
+// GAME 2
+function(){
+	let fahrenheit = document.getElementById('inputF');
+	let celsius = (fahrenheit - 32) / 1.8;
+}
+
