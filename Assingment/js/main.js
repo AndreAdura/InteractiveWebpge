09aa37a1ab21f12bd318745d04a7b36d5843cfac
Andre Henrique Adura <38469671+AndreAdura@users.noreply.github.com>
2018-05-15 12:54:53 -0700

    Update main.js

--- Assingment/js/main.js
+++ Assingment/js/main.js
@@ -1,11 +1,92 @@
 // GAME 1
-function(){
-	document.getElementById('name').innerHTML = "Hello " + input('name') + ", Nice work! Let’s start!";
+function game1(){
+	
+	let name = document.getElementById('name').value;
+	
+
+	document.getElementById('hello').innerHTML = "Hello " + name + ", Nice work! Let's start!";
+
 }
 
 // GAME 2
-function(){
-	let fahrenheit = document.getElementById('inputF');
-	let celsius = (fahrenheit - 32) / 1.8;
+function game2(){
+	 let f = document.getElementById('insertF').value;
+	 let c = (f - 32) / 1.8;
+	document.getElementById('converted').innerHTML = c;
 }
+
+// GAME 4
+function game4(){
+
+let age = document.getElementById('age');
+if(age < 0){
+	alert("you’re kidding, right?");
+} else if(age === 0){
+	alert("oh Baby, how can you work with computer that early in life");
+} else if (age === 100 ) {
+	alert("you are too old to play !");
+} else {
+	alert("you’re good to go!");
+}
+}
+
+// GAME 5
+function game5(){
+
+let number = document.getElementById('number');
+let divisor = 2;
+
+
+if (number < 2 ) {
+	alert("It's not Prime")
+} else if(number = 2){
+	alert("Number is Prime")
+} else if (number > 2) {
+
+	let remainder = number % divisor
+	if(remainder = 0){
+		alert("It's not Prime")
+
+	}else {
+		divisor += 1
+	}
+		if (divisor === number) {
+		alert("number is Prime")
+	}
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+// if(number > 2)
+// remainder = number % divisor
+
+// if(remeinder = 0)
+// print " it's not prime"
+// number + = 1
+// divisor = 2
+// process again
+ 
+//  else 
+//  divisor + = 1
+ 
+//  if(divisor = number)
+//  print " it's prime"
+//  divisor = 2
+//  number + = 1 
+//  process again 
+
+
+
+
 
