8090720c5143e5e02aefce4581ec730606d22365
Andre Henrique Adura <38469671+AndreAdura@users.noreply.github.com>
2018-05-15 17:22:31 -0700

    Update main.js

--- Assingment/js/main.js
+++ Assingment/js/main.js
@@ -18,22 +18,22 @@ function game2(){
 // GAME 4
 function game4(){
 
-let age = document.getElementById('age');
+let age = document.getElementById('age').value;
 if(age < 0){
-	alert("you’re kidding, right?");
-} else if(age === 0){
+	alert("you're kidding, right?");
+} else if(age == 0){
 	alert("oh Baby, how can you work with computer that early in life");
-} else if (age === 100 ) {
+} else if (age == 100 ) {
 	alert("you are too old to play !");
 } else {
-	alert("you’re good to go!");
+	alert("you're good to go!");
 }
 }
 
 // GAME 5
 function game5(){
 
-let number = document.getElementById('number');
+let number = document.getElementById('number').value;
 let divisor = 2;
 
 
@@ -63,30 +63,4 @@ if (number < 2 ) {
 
 
 }
-
-
-
-
-
-// if(number > 2)
-// remainder = number % divisor
-
-// if(remeinder = 0)
-// print " it's not prime"
-// number + = 1
-// divisor = 2
-// process again
- 
-//  else 
-//  divisor + = 1
- 
-//  if(divisor = number)
-//  print " it's prime"
-//  divisor = 2
-//  number + = 1 
-//  process again 
-
-
-
-
 
